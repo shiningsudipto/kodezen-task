aec995bf182b5d7eb1a7046c217e0bb785a4175b
shiningsudipto <shiningsudipto@gmail.com>
2023-10-16 20:20:14 +0600

    left side main action added

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,5 +1,8 @@
 import { AiOutlineMenu } from 'react-icons/ai';
 import { PiDotsNineBold } from 'react-icons/pi';
+import { GrEdit } from 'react-icons/gr';
+import { BsCircleHalf } from 'react-icons/bs';
+import { FcSettings } from 'react-icons/fc';
 const LeftSidebar = () => {
     return (
         <div className='leftSidebar'>
@@ -9,7 +12,11 @@ const LeftSidebar = () => {
                 <PiDotsNineBold className='cursor-pointer' />
             </div>
             <div className='leftSidebarActions'>
-
+                <div className="mainActions">
+                    <GrEdit className='actionIcon' />
+                    <BsCircleHalf className='actionIcon' />
+                    <FcSettings className='actionIcon' />
+                </div>
             </div>
         </div>
     );
